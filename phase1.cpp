cf5e35650b5b13da9c7d7b19d0aa2fb72c159a69
DuBo Lee <dubolee46697@gmail.com>
2026-05-28 21:38:07 +0100

    Complete the phase1.cpp
    Fixed syntax errors and added missing semicolons.

--- phase1.cpp
+++ phase1.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <string>
-using namespace std
+using namespace std;
+
+int main(){
+    int a = 20;
+    double frac = 9.32;
+    char Ce = 'A';
+    string name = "aerwerwerwer";
+    bool active = true;
+    cout << "his number is" << a << endl;
+    cout << "the price" << frac << endl;
+    cout << name << "'s grade is" << Ce << endl;
+    return 0
+}
+
